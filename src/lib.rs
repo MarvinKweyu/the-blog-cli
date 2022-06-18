cf89b8387519caf358139eda4abb1346ad32dbf3
Marvin Kweyu <mkweyu1@gmail.com>
2022-06-18 19:15:51 +0300

    chore: impl review and publishing or post

--- src/lib.rs
+++ src/lib.rs
@@ -19,9 +19,73 @@ impl Post {
             content: String::new(),
         }
     }
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(self)
+    }
+
+    pub fn request_review(&mut self) {
+        // use take() to take some value out of the state field and leave None in its place
+        // since there can be no unpopulated fields in structs, state is taken from Post entirely other than borrowing
+        if let Some(s) = self.state.take() {
+            // call internal request_review on the current state of the post i.e consume current state and return new one
+            self.state = Some(s.request_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
 }
 
-trait State {}
+trait State {
+    // make method only valid on a Box holding the type
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, _post: &'a Post) -> &'a str {
+        ""
+    }
+}
 struct Draft {}
 
-impl State for Draft {}
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct PendingReview {}
+impl State for PendingReview {
+    // return yourself
+    // when we request a review of a post in pendingReview status already, do not re-review
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    // change state to published
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    // return yourself since you cannot re-approve a publisehd post
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+}
